03be457042bafa31def52b642dc933e73642c7b1
Santanu29 <santanubiswas2905@gmail.com>
2022-08-25 19:31:27 +0530

    Footer text

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -145,13 +145,14 @@ const Footer = () => {
 							</a>
 						</section>
 					</div>
-					<div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-						© 2022 Copyright:{" "}
-						<a className="text-white" href="https://nagarro.com/">
-							{" "}
-							nagarro.com{" "}
-						</a>
-					</div>
+					<p className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+						<p>
+							<a className="text-white" href="https://nagarro.com/">
+								Nagarro
+							</a>
+							&nbsp;© 2022. All rights reserved.
+						</p>
+					</p>
 				</footer>
 			</div>
 		</>
