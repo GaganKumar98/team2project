c755aab23994f76fba81ce64e1eea7fe2d940971
Santanu29 <santanubiswas2905@gmail.com>
2022-08-25 19:21:46 +0530

    Cleanup and formatted

--- src/Components/AddQnA.tsx
+++ src/Components/AddQnA.tsx
@@ -1,40 +1,40 @@
 import React from "react";
 
 const AddQnA = () => {
-  return (
-    <>
-      <h2>Add QnA</h2>
-      <div className="container-lg mt-3">
-        <div className="row justify-content-center my-3">
-          <div className="col-lg-6 text-start">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="question" className="form-label">
-                  Question
-                </label>
-                <input type="text" className="form-control" id="question" />
-              </div>
+	return (
+		<>
+			<h2>Add QnA</h2>
+			<div className="container-lg mt-3">
+				<div className="row justify-content-center my-3">
+					<div className="col-lg-6 text-start">
+						<form>
+							<div className="mb-3">
+								<label htmlFor="question" className="form-label">
+									Question
+								</label>
+								<input type="text" className="form-control" id="question" />
+							</div>
 
-              <div className="my-3">
-                <label htmlFor="answer" className="form-label">
-                  Answer
-                </label>
-                <textarea rows={3} className="form-control" id="answer" />
-              </div>
+							<div className="my-3">
+								<label htmlFor="answer" className="form-label">
+									Answer
+								</label>
+								<textarea rows={3} className="form-control" id="answer" />
+							</div>
 
-              <button type="submit" className="btn btn-primary btn-lg">
-                Add QnA
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+							<button type="submit" className="btn btn-primary btn-lg">
+								Add QnA
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AddQnA;
 
 {
-  /* <textarea className="form-control" rows={3} /> */
+	/* <textarea className="form-control" rows={3} /> */
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 
 export const Header = () => {
-
-  return <>
-    <div className="container-fluid d-flex  align-items-center justify-content-between header" style={{ backgroundColor:" rgb(236, 225, 178)"}}>
-      <h4>Team2</h4>
-      <div className='d-flex align-items-center'>
-        <form className="form-inline my-2 my-lg-1 d-flex mx-5">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className='btn' type="submit"><img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E" alt="search"/></button>
-        </form>
-        <button className='btn btnHeader' style={{  backgroundColor: "rgb(127, 189, 255)", margin:"10px"}}><Link to="/add">Add New</Link></button>
-      </div>
-
-    </div>
-  </>;
+	return (
+		<>
+			<div className="container-fluid d-flex  align-items-center justify-content-between header" style={{ backgroundColor: " rgb(236, 225, 178)" }}>
+				<h4>Team2</h4>
+				<div className="d-flex align-items-center">
+					<form className="form-inline my-2 my-lg-1 d-flex mx-5">
+						<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn" type="submit">
+							<img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E" alt="search" />
+						</button>
+					</form>
+					<button className="btn btnHeader" style={{ backgroundColor: "rgb(127, 189, 255)", margin: "10px" }}>
+						<Link to="/add">Add New</Link>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
